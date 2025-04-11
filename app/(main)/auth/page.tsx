c6a38a0e69b9606af04d56793833d19339cf2453
TecTrix <93141222+tectrixdev@users.noreply.github.com>
2025-04-12 00:36:46 +0200

    no-cors

--- app/(main)/auth/page.tsx
+++ app/(main)/auth/page.tsx
@@ -2,22 +2,23 @@
 import GetSearchParams from "@/components/getsearchparams";
 
 export default async function page() {
-  const fetchurl = `https://github.com/login/oauth/access_token?client_id=Iv23lilTSFxvqmY2Ojft&client_secret=${process.env.client_secret}&code=${GetSearchParams}`;
-  const response = await fetch(fetchurl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  });
+    const fetchurl = `https://github.com/login/oauth/access_token?client_id=Iv23lilTSFxvqmY2Ojft&client_secret=${process.env.client_secret}&code=${GetSearchParams}`;
+    const response = await fetch(fetchurl, {
+        method: "POST",
+        mode: "no-cors", // Add no-cors mode
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+    });
 
-  if (response.ok) {
-    const data = await response.json();
-    const accessToken = data.access_token;
-    return accessToken;
-    console.log("success");
-  } else {
-    return "Error fetching access token";
-    console.log("Error fetching access token");
-  }
+    if (response.ok) {
+        const data = await response.json();
+        const accessToken = data.access_token;
+        console.log("success");
+        return accessToken;
+    } else {
+        console.log("Error fetching access token");
+        return "Error fetching access token";
+    }
 }
