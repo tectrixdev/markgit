d969a9126f5717f6eaa3ef44f478fa2b8c1495c6
TecTrix <93141222+tectrixdev@users.noreply.github.com>
2025-04-11 23:32:12 +0200

    add hopefully working auth with risky enviroment variable leakage

--- app/(main)/auth/page.tsx
+++ app/(main)/auth/page.tsx
@@ -1,9 +1,11 @@
-"use server";
+"use client";
+import { useSearchParams } from "next/navigation";
 
 export default async function page() {
+  const param = useSearchParams().get("code");
   await fetch(
-    `https://github.com/login/oauth/access_token?client_id=Iv23lilTSFxvqmY2Ojft&client_secret="
-        ${process.env.client_secret}`,
+    `https://github.com/login/oauth/access_token?client_id=Iv23lilTSFxvqmY2Ojft&client_secret=
+        ${process.env.client_secret}&code=${param}`,
     {
       method: "POST",
       headers: {
